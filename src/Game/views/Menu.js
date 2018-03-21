fe4ee8d0574abcf924dd47abf37adb129f99930e
Dim Dim <Dim Dim>
2018-03-21 17:27:32 +0200

    fix

--- src/Game/views/Menu.js
+++ src/Game/views/Menu.js
@@ -7,7 +7,7 @@ class Menu extends PIXI.Container{
 		this.stage = app.stage;
 		this.buttons = require('./menuConfigs/'+config).default || [];
 		this.calculatePositions();
-		this.buttons.forEach((e,i) => this.addChild(e));
+		this.buttons.forEach((e) => this.addChild(e));
 		this.test = "dsa";
 		this.position.set(app.screen.width/2, app.screen.height/2);
 	}
