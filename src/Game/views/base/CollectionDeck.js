5b40bc428f60336a1e31984cdc6f491384aa1cd5
Dim Dim <Dim Dim>
2018-04-18 19:08:13 +0300

    Collection deck init

--- src/Game/views/base/CollectionDeck.js
+++ src/Game/views/base/CollectionDeck.js
@@ -1,11 +1,12 @@
 import * as PIXI from 'pixi.js';
 import GuiableContainer from '../../../helpers/Guiable';
+import CardHolder from '../base/CardHolder';
 import Card from '../base/Card';
 
 const DefaultImageUrl = '/files/assets/ui/papyrus.jpg';
 const DefaultImage = PIXI.Texture.fromImage(DefaultImageUrl);
 
-class CardCollection extends GuiableContainer{
+class CollectionDeck extends GuiableContainer{
     constructor(props){
         super(props);
         let {
@@ -21,17 +22,18 @@ class CardCollection extends GuiableContainer{
         this.x = x || 0;
         this.y = y || 0;
 
-          // GUI
-        this.addFolder('CardCollection');
-        this.addToFolder('CardCollection', this, 'imageURL').onFinishChange((v) => this.loadImage(v));
-        this.addToFolder('CardCollection', this, 'x').onFinishChange((v) => this.position.x = v);
-        this.addToFolder('CardCollection', this, 'y').onFinishChange((v) => this.position.y = v);
+        // GUI
+        this.addFolder('CollectionDeck');
+        this.addToFolder('CollectionDeck', this, 'imageURL').onFinishChange((v) => this.loadImage(v));
+        this.addToFolder('CollectionDeck', this, 'x').onFinishChange((v) => this.position.x = v);
+        this.addToFolder('CollectionDeck', this, 'y').onFinishChange((v) => this.position.y = v);
         //
 
         this.construct(props);
     }
+
     construct(props){
-        let [w,h] = [800,600];
+        let [w,h] = [200,600];
 
         this.sprite = new PIXI.Sprite(DefaultImage);
         this.sprite.anchor.set(0.5,0.5);
@@ -40,16 +42,16 @@ class CardCollection extends GuiableContainer{
 
         this.addChild(this.sprite);
 
-        let card = new Card({'x':2*w/3, 'y':-h/2});
-        this.sprite.addChild(card);
-        let card2 = new Card({'x':w/3, 'y':-h/2});
-        this.sprite.addChild(card2);
-        let card3 = new Card({'x':0, 'y':-h/2});
-        this.sprite.addChild(card3);
-        let card4 = new Card({'x':-2*w/3, 'y':-h/2});
-        this.sprite.addChild(card4);
-        let card5 = new Card({'x':-w/3, 'y':-h/2});
-        this.sprite.addChild(card5);
+        let cardHolder = new CardHolder({'x':0,'y':0,'w':60,'h':96});
+        this.addChild(cardHolder);
+        let cardHolder2 = new CardHolder({'x':0,'y':100,'w':60,'h':96});
+        this.addChild(cardHolder2);
+        let cardHolder3 = new CardHolder({'x':0,'y':0,'w':60,'h':96});
+        this.addChild(cardHolder3);
+        let cardHolder4 = new CardHolder({'x':0,'y':0,'w':60,'h':96});
+        this.addChild(cardHolder4);
+        let cardHolder5 = new CardHolder({'x':0,'y':0,'w':60,'h':96});
+        this.addChild(cardHolder5);
 
 
 
@@ -70,13 +72,12 @@ class CardCollection extends GuiableContainer{
 
     getAsJSON() {
         return {
-            component: 'base/CardCollection',
+            component: 'base/CollectionDeck',
             image: this.imageURL,
             x: this.position.x,
             y: this.position.y
         };
     }
-
 }
 
-export default CardCollection;
+export default CollectionDeck;
---
+++ src/Game/views/demo/CardCollectionDemo.js
@@ -0,0 +1,19 @@
+import * as PIXI from 'pixi.js';
+
+import CardCollection from '../base/CardCollection';
+import CollectionDeck from '../base/CollectionDeck';
+
+class CardCollectionDemo extends PIXI.Container{
+	constructor(){
+		super();
+
+		let cardCollection = new CardCollection({});
+		this.addChild(cardCollection);
+		let collectionDeck = new CollectionDeck({'x': 600, 'y':0 });
+		this.addChild(collectionDeck);
+	}
+
+	getAsJSON = () => {return {component: 'demo/CardCollectionDemo'}}
+}
+
+export default CardCollectionDemo;
